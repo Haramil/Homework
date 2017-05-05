cda8ccc5fe369fa4e5049d4b0aea795cd3b1717b
Рамиль <@RAMIL-PC>
2017-05-05 21:57:49 +0400

    menu added

---
+++ TicTacToe/TicTacToe/TicTacToeForm.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToeForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cellButton1 = new System.Windows.Forms.Button();
+            this.cellButton2 = new System.Windows.Forms.Button();
+            this.cellButton3 = new System.Windows.Forms.Button();
+            this.cellButton6 = new System.Windows.Forms.Button();
+            this.cellButton5 = new System.Windows.Forms.Button();
+            this.cellButton4 = new System.Windows.Forms.Button();
+            this.cellButton9 = new System.Windows.Forms.Button();
+            this.cellButton8 = new System.Windows.Forms.Button();
+            this.cellButton7 = new System.Windows.Forms.Button();
+            this.gameFieldGroupBox = new System.Windows.Forms.GroupBox();
+            this.ticRadioButton = new System.Windows.Forms.RadioButton();
+            this.tacRadioButton = new System.Windows.Forms.RadioButton();
+            this.selectTicTacGroupBox = new System.Windows.Forms.GroupBox();
+            this.gameModeGroupBox = new System.Windows.Forms.GroupBox();
+            this.singlePlayerRadioButton = new System.Windows.Forms.RadioButton();
+            this.multiPlayerRadioButton = new System.Windows.Forms.RadioButton();
+            this.startButton = new System.Windows.Forms.Button();
+            this.gameFieldGroupBox.SuspendLayout();
+            this.selectTicTacGroupBox.SuspendLayout();
+            this.gameModeGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cellButton1
+            // 
+            this.cellButton1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton1.Location = new System.Drawing.Point(20, 25);
+            this.cellButton1.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton1.Name = "cellButton1";
+            this.cellButton1.Size = new System.Drawing.Size(100, 100);
+            this.cellButton1.TabIndex = 0;
+            this.cellButton1.TabStop = false;
+            this.cellButton1.UseVisualStyleBackColor = false;
+            // 
+            // cellButton2
+            // 
+            this.cellButton2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton2.Location = new System.Drawing.Point(120, 25);
+            this.cellButton2.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton2.Name = "cellButton2";
+            this.cellButton2.Size = new System.Drawing.Size(100, 100);
+            this.cellButton2.TabIndex = 1;
+            this.cellButton2.TabStop = false;
+            this.cellButton2.UseVisualStyleBackColor = false;
+            // 
+            // cellButton3
+            // 
+            this.cellButton3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton3.Location = new System.Drawing.Point(220, 25);
+            this.cellButton3.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton3.Name = "cellButton3";
+            this.cellButton3.Size = new System.Drawing.Size(100, 100);
+            this.cellButton3.TabIndex = 2;
+            this.cellButton3.TabStop = false;
+            this.cellButton3.UseVisualStyleBackColor = false;
+            // 
+            // cellButton6
+            // 
+            this.cellButton6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton6.Location = new System.Drawing.Point(220, 125);
+            this.cellButton6.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton6.Name = "cellButton6";
+            this.cellButton6.Size = new System.Drawing.Size(100, 100);
+            this.cellButton6.TabIndex = 5;
+            this.cellButton6.TabStop = false;
+            this.cellButton6.UseVisualStyleBackColor = false;
+            // 
+            // cellButton5
+            // 
+            this.cellButton5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton5.Location = new System.Drawing.Point(120, 125);
+            this.cellButton5.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton5.Name = "cellButton5";
+            this.cellButton5.Size = new System.Drawing.Size(100, 100);
+            this.cellButton5.TabIndex = 4;
+            this.cellButton5.TabStop = false;
+            this.cellButton5.UseVisualStyleBackColor = false;
+            // 
+            // cellButton4
+            // 
+            this.cellButton4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton4.Location = new System.Drawing.Point(20, 125);
+            this.cellButton4.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton4.Name = "cellButton4";
+            this.cellButton4.Size = new System.Drawing.Size(100, 100);
+            this.cellButton4.TabIndex = 3;
+            this.cellButton4.TabStop = false;
+            this.cellButton4.UseVisualStyleBackColor = false;
+            // 
+            // cellButton9
+            // 
+            this.cellButton9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton9.Location = new System.Drawing.Point(220, 225);
+            this.cellButton9.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton9.Name = "cellButton9";
+            this.cellButton9.Size = new System.Drawing.Size(100, 100);
+            this.cellButton9.TabIndex = 8;
+            this.cellButton9.TabStop = false;
+            this.cellButton9.UseVisualStyleBackColor = false;
+            // 
+            // cellButton8
+            // 
+            this.cellButton8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton8.Location = new System.Drawing.Point(120, 225);
+            this.cellButton8.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton8.Name = "cellButton8";
+            this.cellButton8.Size = new System.Drawing.Size(100, 100);
+            this.cellButton8.TabIndex = 7;
+            this.cellButton8.TabStop = false;
+            this.cellButton8.UseVisualStyleBackColor = false;
+            // 
+            // cellButton7
+            // 
+            this.cellButton7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cellButton7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PowderBlue;
+            this.cellButton7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.cellButton7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cellButton7.Location = new System.Drawing.Point(20, 225);
+            this.cellButton7.Margin = new System.Windows.Forms.Padding(0);
+            this.cellButton7.Name = "cellButton7";
+            this.cellButton7.Size = new System.Drawing.Size(100, 100);
+            this.cellButton7.TabIndex = 6;
+            this.cellButton7.TabStop = false;
+            this.cellButton7.UseVisualStyleBackColor = false;
+            // 
+            // gameFieldGroupBox
+            // 
+            this.gameFieldGroupBox.Controls.Add(this.cellButton1);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton9);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton2);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton8);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton3);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton7);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton4);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton6);
+            this.gameFieldGroupBox.Controls.Add(this.cellButton5);
+            this.gameFieldGroupBox.Enabled = false;
+            this.gameFieldGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.gameFieldGroupBox.Name = "gameFieldGroupBox";
+            this.gameFieldGroupBox.Size = new System.Drawing.Size(340, 345);
+            this.gameFieldGroupBox.TabIndex = 9;
+            this.gameFieldGroupBox.TabStop = false;
+            this.gameFieldGroupBox.Text = "Игра";
+            // 
+            // ticRadioButton
+            // 
+            this.ticRadioButton.AutoSize = true;
+            this.ticRadioButton.Checked = true;
+            this.ticRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.ticRadioButton.Name = "ticRadioButton";
+            this.ticRadioButton.Size = new System.Drawing.Size(87, 17);
+            this.ticRadioButton.TabIndex = 11;
+            this.ticRadioButton.TabStop = true;
+            this.ticRadioButton.Text = "за крестики";
+            this.ticRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // tacRadioButton
+            // 
+            this.tacRadioButton.AutoSize = true;
+            this.tacRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.tacRadioButton.Name = "tacRadioButton";
+            this.tacRadioButton.Size = new System.Drawing.Size(76, 17);
+            this.tacRadioButton.TabIndex = 12;
+            this.tacRadioButton.Text = "за нолики";
+            this.tacRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // selectTicTacGroupBox
+            // 
+            this.selectTicTacGroupBox.Controls.Add(this.ticRadioButton);
+            this.selectTicTacGroupBox.Controls.Add(this.tacRadioButton);
+            this.selectTicTacGroupBox.Location = new System.Drawing.Point(358, 87);
+            this.selectTicTacGroupBox.Name = "selectTicTacGroupBox";
+            this.selectTicTacGroupBox.Size = new System.Drawing.Size(200, 69);
+            this.selectTicTacGroupBox.TabIndex = 13;
+            this.selectTicTacGroupBox.TabStop = false;
+            this.selectTicTacGroupBox.Text = "Выберите, за кого играть";
+            // 
+            // gameModeGroupBox
+            // 
+            this.gameModeGroupBox.Controls.Add(this.singlePlayerRadioButton);
+            this.gameModeGroupBox.Controls.Add(this.multiPlayerRadioButton);
+            this.gameModeGroupBox.Location = new System.Drawing.Point(358, 12);
+            this.gameModeGroupBox.Name = "gameModeGroupBox";
+            this.gameModeGroupBox.Size = new System.Drawing.Size(200, 69);
+            this.gameModeGroupBox.TabIndex = 14;
+            this.gameModeGroupBox.TabStop = false;
+            this.gameModeGroupBox.Text = "Выберите режим игры";
+            // 
+            // singlePlayerRadioButton
+            // 
+            this.singlePlayerRadioButton.AutoSize = true;
+            this.singlePlayerRadioButton.Checked = true;
+            this.singlePlayerRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.singlePlayerRadioButton.Name = "singlePlayerRadioButton";
+            this.singlePlayerRadioButton.Size = new System.Drawing.Size(105, 17);
+            this.singlePlayerRadioButton.TabIndex = 11;
+            this.singlePlayerRadioButton.TabStop = true;
+            this.singlePlayerRadioButton.Text = "с компьютером";
+            this.singlePlayerRadioButton.UseVisualStyleBackColor = true;
+            this.singlePlayerRadioButton.CheckedChanged += new System.EventHandler(this.singlePlayerRadioButton_CheckedChanged);
+            // 
+            // multiPlayerRadioButton
+            // 
+            this.multiPlayerRadioButton.AutoSize = true;
+            this.multiPlayerRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.multiPlayerRadioButton.Name = "multiPlayerRadioButton";
+            this.multiPlayerRadioButton.Size = new System.Drawing.Size(89, 17);
+            this.multiPlayerRadioButton.TabIndex = 12;
+            this.multiPlayerRadioButton.Text = "с человеком";
+            this.multiPlayerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(358, 162);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(200, 23);
+            this.startButton.TabIndex = 15;
+            this.startButton.Text = "Начать игру";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // TicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(892, 511);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.gameModeGroupBox);
+            this.Controls.Add(this.selectTicTacGroupBox);
+            this.Controls.Add(this.gameFieldGroupBox);
+            this.Name = "TicTacToeForm";
+            this.Text = "Крестики-нолики";
+            this.gameFieldGroupBox.ResumeLayout(false);
+            this.selectTicTacGroupBox.ResumeLayout(false);
+            this.selectTicTacGroupBox.PerformLayout();
+            this.gameModeGroupBox.ResumeLayout(false);
+            this.gameModeGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cellButton1;
+        private System.Windows.Forms.Button cellButton2;
+        private System.Windows.Forms.Button cellButton3;
+        private System.Windows.Forms.Button cellButton6;
+        private System.Windows.Forms.Button cellButton5;
+        private System.Windows.Forms.Button cellButton4;
+        private System.Windows.Forms.Button cellButton9;
+        private System.Windows.Forms.Button cellButton8;
+        private System.Windows.Forms.Button cellButton7;
+        private System.Windows.Forms.GroupBox gameFieldGroupBox;
+        private System.Windows.Forms.RadioButton ticRadioButton;
+        private System.Windows.Forms.RadioButton tacRadioButton;
+        private System.Windows.Forms.GroupBox selectTicTacGroupBox;
+        private System.Windows.Forms.GroupBox gameModeGroupBox;
+        private System.Windows.Forms.RadioButton singlePlayerRadioButton;
+        private System.Windows.Forms.RadioButton multiPlayerRadioButton;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/TicTacToeForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToeForm : Form
+    {
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void singlePlayerRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            selectTicTacGroupBox.Enabled = singlePlayerRadioButton.Checked;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            gameFieldGroupBox.Enabled = true;
+        }
+    }
+}
