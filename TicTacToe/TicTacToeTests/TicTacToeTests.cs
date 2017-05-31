cfcc5361efe69737278702c55b6617757778d3ad
Рамиль <@RAMIL-PC>
2017-05-31 17:23:57 +0400

    added tests project

---
+++ TicTacToe/TicTacToeTests/TicTacToeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToeTests
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
